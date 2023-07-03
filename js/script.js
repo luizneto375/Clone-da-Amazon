2f18bac90f99453855017eaba41fd860199adb4a
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-07-03 15:41:35 -0300

    add cardes a ajustando tamanhos

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*let angle = 0;
+function sliderImagens(sign) {
+    slider = document.querySelector("#slider");
+    if (sign) { angle = angle + 45; } else { angle = angle - 45; }
+    slider.setAttribute("style","-webkit-transform: rotatey("+angle +"deg); -moz-transform: rotatey("+angle +"deg); transfomr: rotatey("+angle +"deg);")
+}*/
