a62037a997859ad6430e789d2180fe54c342a278
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-07-05 20:13:01 -0300

    refazendo projeto pra ajustar os elementos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*let angle = 0;
-function sliderImagens(sign) {
+/*function sliderImagens(sign) {
     slider = document.querySelector("#slider");
     if (sign) { angle = angle + 45; } else { angle = angle - 45; }
     slider.setAttribute("style","-webkit-transform: rotatey("+angle +"deg); -moz-transform: rotatey("+angle +"deg); transfomr: rotatey("+angle +"deg);")
