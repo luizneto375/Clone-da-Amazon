9863d5aeaedd077121c03c4f7f0f71c306a10ad6
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-07-14 16:08:23 -0300

    adicioando o footer, e o js para fazer a interatividade

--- js/apps.js
+++ js/apps.js
@@ -31,3 +31,42 @@ lef1tbtn.addEventListener('click', function(event){
 })
 
 
+const backtop = document.querySelector(".backtop");
+
+backtop.addEventListener("clkick", ()=>{
+    document.scrollTop({
+        top: 0,
+        behavior: "smooth"
+    });
+})
+
+const sidebar = document.querySelector(".sidebar");
+const cross = document.querySelector(".fa-xmark");
+const black = document.querySelector(".black");
+const sidebtn = document.querySelector(".second-1");
+
+sidebtn.addEventListener("click", ()=>{
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+    document.body.classList.add("stop-scroll");
+})
+
+cross.addEventListener("click", ()=>{
+    sidebar.classList.remove("active");
+    cross.classList.remove("active");
+    black.classList.remove("active");
+    document.body.classList.add("stop-scroll");
+})
+
+const sign = document.querySelector(".ac");
+const tri = document.querySelector(".triangle");
+const signin = document.querySelector(".hdn-sign");
+
+
+sign.addEventListener("click", ()=>{
+    black.classList.toggle("active-1");
+    signin.classList.toggle("active");
+    tri.classList.toggle("active");
+    
+})
