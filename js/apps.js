d4dddaf86414341aea9ca20239108e6abdc8b9fd
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-07-14 21:11:37 -0300

    fazendo testes no js

--- js/apps.js
+++ js/apps.js
@@ -33,8 +33,8 @@ lef1tbtn.addEventListener('click', function(event){
 
 const backtop = document.querySelector(".backtop");
 
-backtop.addEventListener("clkick", ()=>{
-    document.scrollTop({
+backtop.addEventListener("clkick", ()=> {
+    document.scrollTo({
         top: 0,
         behavior: "smooth"
     });
@@ -45,18 +45,18 @@ const cross = document.querySelector(".fa-xmark");
 const black = document.querySelector(".black");
 const sidebtn = document.querySelector(".second-1");
 
-sidebtn.addEventListener("click", ()=>{
+sidebtn.addEventListener("click", ()=> {
     sidebar.classList.add("active");
     cross.classList.add("active");
     black.classList.add("active");
     document.body.classList.add("stop-scroll");
 })
 
-cross.addEventListener("click", ()=>{
+cross.addEventListener("click", ()=> {
     sidebar.classList.remove("active");
     cross.classList.remove("active");
     black.classList.remove("active");
-    document.body.classList.add("stop-scroll");
+    document.body.classList.remove("stop-scroll");
 })
 
 const sign = document.querySelector(".ac");
@@ -64,9 +64,9 @@ const tri = document.querySelector(".triangle");
 const signin = document.querySelector(".hdn-sign");
 
 
-sign.addEventListener("click", ()=>{
+sign.addEventListener("click", ()=> {
     black.classList.toggle("active-1");
     signin.classList.toggle("active");
     tri.classList.toggle("active");
-    
+    document.body.classList.toggle("stop-scroll");
 })
